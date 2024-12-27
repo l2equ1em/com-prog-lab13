a4db7b9c94a44f378d73a1a1063181aacca359a7
LAB303-24 <lab303-24@vdieng.local>
2024-12-27 15:48:03 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,21 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double num[],int n,double output[]){
+    double sum = 0, product = 1.0, sum2 = 0, sumH = 0, max = num[0], min = num[0];
+    for(int i = 0; i < n; i++){
+        sum += num[i];
+        sum2 += num[i]*num[i];
+        product *= num[i];
+        sumH += 1/num[i];
+        if(num[i] > max) max = num[i];
+        else if(num[i] < min) min = num[i];
+    }
+    output[0] = sum / n;
+    output[1] = sqrt(sum2 / n - (sum / n) * (sum / n));
+    output[2] = pow(product, 1.0 / n);
+    output[3] = n / sumH;
+    output[4] = max;
+    output[5] = min;
+}
